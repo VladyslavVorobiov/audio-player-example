b232e517955c79bc3c8835fddaff092c7470b7e2
Vladyslav Vorobiov <vlad65e@gmail.com>
2023-07-26 22:46:45 +0300

    change to class syntax

--- e_book_audio_player.js
+++ e_book_audio_player.js
@@ -33,92 +33,103 @@ function initAudioPlayer() {
 
 };
 
-function AudioPlayerConstructor(id) {
-    this.audioPlayerRef = document.querySelector(`div[id=${id}]`);
+class AudioPlayerConstructor {
 
-    this.currentTimeRef = this.audioPlayerRef.querySelector('.controls__current-time');
-    this.totalTimeRef = this.audioPlayerRef.querySelector('.controls__total-time');
-    this.progressRef = this.audioPlayerRef.querySelector('.controls__progress-range');
-    this.loaderRef = this.audioPlayerRef.querySelector('.loading');
-    this.playPauseButtonRef = this.audioPlayerRef.querySelector('.play-pause-btn');
-    this.controlsWrapperRef = this.audioPlayerRef.querySelector('.controls');
-    this.playPauseButtonPath = this.playPauseButtonRef.querySelector('path');
-    this.volumeButtonRef = this.audioPlayerRef.querySelector('.volume__button');
-    this.volumeControlsRef = this.audioPlayerRef.querySelector('.volume__controls');
-    this.volumeLevelRef = this.audioPlayerRef.querySelector('.volume__progress');
-};
+    constructor(id){
+        this.audioPlayerRef = document.querySelector(`div[id=${id}]`);
 
-AudioPlayerConstructor.prototype.setCurrentTime = setCurrentTime;
-AudioPlayerConstructor.prototype.setTotalTime = setTotalTime;
-AudioPlayerConstructor.prototype.setProgress = setProgress;
-AudioPlayerConstructor.prototype.setVolumeLevel = setVolumeLevel;
-AudioPlayerConstructor.prototype.toggleLoading = toggleLoading;
-AudioPlayerConstructor.prototype.togglePlayPauseButton = togglePlayPauseButton;
-AudioPlayerConstructor.prototype.initListeners = initListeners;
-AudioPlayerConstructor.prototype.removeListeners = removeListeners;
-AudioPlayerConstructor.prototype.onPlayClicked = onPlayClicked;
-AudioPlayerConstructor.prototype.onPauseClicked = onPauseClicked;
-AudioPlayerConstructor.prototype.onProgressChanged = onProgressChanged;
-AudioPlayerConstructor.prototype.onVolumeLevelClicked = onVolumeLevelClicked;
-
-// time type: string like '2:59'
-function setCurrentTime(time) {
-    this.currentTimeRef.textContent = time;
-};
-
-// time type: string like '10:00'
-function setTotalTime(time) {
-    this.totalTimeRef.textContent = time;
-};
-
-// progress type: number between 0 and 100
-function setProgress(progress) {
-    this.progressRef.value = progress;
-};
-
-// level type: number between 0 and 100
-function setVolumeLevel(level) {
-    this.volumeLevelRef.value = level;
-};
-
-// loading type: boolean
-function toggleLoading(loading) {
-    if(loading) {
-        this.loaderRef.style.display = 'block';
-        this.playPauseButtonRef.disabled = true;
-        this.progressRef.disabled = true;
-        this.controlsWrapperRef.classList.add('disabled');
-    } else {
-        this.loaderRef.style.display = 'none';
-        this.playPauseButtonRef.disabled = false;
-        this.progressRef.disabled = false;
-        this.controlsWrapperRef.classList.remove('disabled');
+        this.currentTimeRef = this.audioPlayerRef.querySelector('.controls__current-time');
+        this.totalTimeRef = this.audioPlayerRef.querySelector('.controls__total-time');
+        this.progressRef = this.audioPlayerRef.querySelector('.controls__progress-range');
+        this.loaderRef = this.audioPlayerRef.querySelector('.loading');
+        this.playPauseButtonRef = this.audioPlayerRef.querySelector('.play-pause-btn');
+        this.controlsWrapperRef = this.audioPlayerRef.querySelector('.controls');
+        this.playPauseButtonPath = this.playPauseButtonRef.querySelector('path');
+        this.volumeButtonRef = this.audioPlayerRef.querySelector('.volume__button');
+        this.volumeControlsRef = this.audioPlayerRef.querySelector('.volume__controls');
+        this.volumeLevelRef = this.audioPlayerRef.querySelector('.volume__progress');
     }
-};
 
-function togglePlayPauseButton() {
-    var buttonAppearance = this.playPauseButtonPath.getAttribute('d');
-
-    if(buttonAppearance === PLAY_BUTTON_APPEARANCE) {
-        this.playPauseButtonPath.setAttribute('d', PAUSE_BUTTON_APPEARANCE);
-    } else {
-        this.playPauseButtonPath.setAttribute('d', PLAY_BUTTON_APPEARANCE);
+    // time type: string like '2:59'
+    setCurrentTime(time) {
+        this.currentTimeRef.textContent = time;
     }
-}
 
-function initListeners() {
-    this.playPauseButtonRef.addEventListener('click', playPauseHandler.bind(this));
-    this.volumeButtonRef.addEventListener('click', volumeClickHandler.bind(this));
-    this.progressRef.addEventListener('change', progressChangesHandler.bind(this));
-    this.volumeLevelRef.addEventListener('change', volumeLevelChangesHandler.bind(this));
-}
+    // time type: string like '10:00'
+    setTotalTime(time) {
+        this.totalTimeRef.textContent = time;
+    }
 
-function removeListeners() {
-    this.playPauseButtonRef.removeEventListener('click', playPauseHandler);
-    this.volumeButtonRef.removeEventListener('click', volumeClickHandler);
-    this.progressRef.removeEventListener('change', progressChangesHandler);
-    this.volumeLevelRef.removeEventListener('change', volumeLevelChangesHandler);
-}
+    // progress type: number between 0 and 100
+    setProgress(progress) {
+        this.progressRef.value = progress;
+    }
+
+    // level type: number between 0 and 100
+    setVolumeLevel(level) {
+        this.volumeLevelRef.value = level;
+    }
+
+    // loading type: boolean
+    toggleLoading(loading) {
+        if(loading) {
+            this.loaderRef.style.display = 'block';
+            this.playPauseButtonRef.disabled = true;
+            this.progressRef.disabled = true;
+            this.controlsWrapperRef.classList.add('disabled');
+        } else {
+            this.loaderRef.style.display = 'none';
+            this.playPauseButtonRef.disabled = false;
+            this.progressRef.disabled = false;
+            this.controlsWrapperRef.classList.remove('disabled');
+        }
+    }
+
+    togglePlayPauseButton() {
+        var buttonAppearance = this.playPauseButtonPath.getAttribute('d');
+    
+        if(buttonAppearance === PLAY_BUTTON_APPEARANCE) {
+            this.playPauseButtonPath.setAttribute('d', PAUSE_BUTTON_APPEARANCE);
+        } else {
+            this.playPauseButtonPath.setAttribute('d', PLAY_BUTTON_APPEARANCE);
+        }
+    }
+    
+    initListeners() {
+        this.playPauseButtonRef.addEventListener('click', playPauseHandler.bind(this));
+        this.volumeButtonRef.addEventListener('click', volumeClickHandler.bind(this));
+        this.progressRef.addEventListener('change', progressChangesHandler.bind(this));
+        this.volumeLevelRef.addEventListener('change', volumeLevelChangesHandler.bind(this));
+    }
+    
+    removeListeners() {
+        this.playPauseButtonRef.removeEventListener('click', playPauseHandler);
+        this.volumeButtonRef.removeEventListener('click', volumeClickHandler);
+        this.progressRef.removeEventListener('change', progressChangesHandler);
+        this.volumeLevelRef.removeEventListener('change', volumeLevelChangesHandler);
+    }
+
+    onPlayClicked() {
+        //TODO place your logic here on play button clicked
+        this.togglePlayPauseButton();
+    }
+    
+    onPauseClicked() {
+        //TODO place your logic here on pause button clicked
+        this.togglePlayPauseButton();
+    }
+    
+    // progress type: number between 0 and 100
+    onProgressChanged(progress) {
+        //TODO place your logic here on progress changed
+    }
+    
+    // level type: number between 0 and 100
+    onVolumeLevelClicked(level) {
+        //TODO place your logic here on volume level clicked
+    }
+
+};
 
 function playPauseHandler() {
     var buttonAppearance = this.playPauseButtonPath.getAttribute('d');
@@ -141,24 +152,4 @@ function progressChangesHandler(event) {
 
 function volumeLevelChangesHandler(event) {
     this.onVolumeLevelClicked(+event.target.value);
-}
-
-function onPlayClicked() {
-    //TODO place your logic here on play button clicked
-    this.togglePlayPauseButton();
-}
-
-function onPauseClicked() {
-    //TODO place your logic here on pause button clicked
-    this.togglePlayPauseButton();
-}
-
-// progress type: number between 0 and 100
-function onProgressChanged(progress) {
-    //TODO place your logic here on progress changed
-}
-
-// level type: number between 0 and 100
-function onVolumeLevelClicked(level) {
-    //TODO place your logic here on volume level clicked
 }
